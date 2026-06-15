8654d2e0f0e900a7817b10cd34f5cf8155dcd017
Mchang-student <mchang@students.mags.school.nz>
2026-06-15 21:56:25 +1200

    Animation + health

--- financially-unstable/scripts/player.gd
+++ financially-unstable/scripts/player.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
 var speed: float = 300.0
-@export var pivot: Node2D
+var health : int = 100
 
+@export var pivot: Node2D
+@export var animated_sprite: AnimatedSprite2D
+@export var health_ui: ProgressBar
+@export var health_label : Label
+
+func _ready() -> void:
+	health_ui.max_value = health
+	health_ui.value = health
+	health_label.text = "" + str(health) + "/" + str(health) 
+	
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2(0.0, 0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
@@ -11,3 +21,19 @@ func _process(delta: float) -> void:
 	move_and_slide()
 	
 	pivot.look_at(get_global_mouse_position())
+	
+	if direction != Vector2.ZERO:
+		animated_sprite.play("default")
+		if direction.x > 0:
+			animated_sprite.flip_h = true
+		elif direction.x < 0:
+			animated_sprite.flip_h = false
+	else: animated_sprite.play("idle")
+
+func take_damage() -> void:
+	if health > 1:
+		health -= 1
+		health_label.text = "" + str(health) + "/10"
+		
+	else: 
+		get_tree.call_deferred("reload_current_scene")
