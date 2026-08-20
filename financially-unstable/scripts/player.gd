c2c667f2d7a4725d6269c3f63c2a41decc1675c1
Mchang-student <mchang@students.mags.school.nz>
2026-08-21 10:05:23 +1200

    Added level 2 node

--- financially-unstable/scripts/player.gd
+++ financially-unstable/scripts/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-var speed: float = 400.0
-var health : int = 100
-var is_punching: bool = false
-var rage: float = 0.0
-var max_rage : float = 100.0
-var is_enraged : bool = false
+var speed: float = 400.0		#player movement speed
+var health : int = 100  		#current health
+var is_punching: bool = false	#tracks if the punch animation is currently playing
+var rage: float = 0.0			#current rage meter value
+var max_rage : float = 100.0	#rage meter cap
+var is_enraged : bool = false	#tracks if rage mode is active
 
 
 
-@export var pivot: Node2D
+@export var pivot: Node2D		#
 @export var animated_sprite: AnimatedSprite2D
 @export var health_ui: ProgressBar
 @export var health_label : Label
@@ -17,6 +17,7 @@ var is_enraged : bool = false
 @export var rage_ui : ProgressBar
 
 func _ready() -> void:
+	#set up health bar and label with starting values
 	health_ui.max_value = health
 	health_ui.value = health
 	health_label.text = "" + str(health) + "/" + str(health) 
@@ -27,35 +28,38 @@ func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2(0.0, 0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-	velocity = speed * direction.normalized()
+	velocity = speed * direction.normalized()		#apply movement
 	move_and_slide()
 	
-	pivot.look_at(get_global_mouse_position())
+	pivot.look_at(get_global_mouse_position())		#rotate the pivot to always face the mouse
 	
-	if direction != Vector2.ZERO and not is_punching:
+	if direction != Vector2.ZERO and not is_punching: #play the movement or idle animation, but don't interupt the punch animationn
 		animated_sprite.play("default")
 	elif not is_punching:
 		animated_sprite.play("idle")
 	
+	# flip sprite to face the direction of the mouse (left/right), unless punching 
 	if not is_punching:
 		if get_global_mouse_position().x < global_position.x:
 			animated_sprite.flip_h = true
 		else:
 			animated_sprite.flip_h = false
 	
+	#trigger punch on input, only if not already punching
 	if Input.is_action_just_pressed("ui_punch") and not is_punching:
 		_start_punch()
-		
+
 func _start_punch() -> void:
 	is_punching = true
 	print("playing punching animation")
 	animated_sprite.play("punch")
 	punch_collision.disabled = false
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.2).timeout #hitbox stays active for 0.2 seconds
 	punch_collision.disabled = true
 	is_punching = false
 
 func take_damage(amount: int) -> void:
+	#take damage when hit, reload the scene when character dies.
 	if health > amount:
 		health -= amount
 		health_label.text = "" + str(health) + "/100"
